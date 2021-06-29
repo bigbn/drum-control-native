fb55863716d30b5ff0d09dca20bc42b5af1ca9dd
Pavel Shabardin <pavel@scat.su>
2021-06-29 20:42:29 +0400

    [upd] Running in joined thread

--- src/main.cc
+++ src/main.cc
@@ -2,16 +2,27 @@
 
 #include <iostream>
 #include <libexadrums/Api/eXaDrums.hpp>
+#include <thread>
 
 // using namespace eXaDrumsApi;
+using namespace std;
+
+void loop_thread(eXaDrumsApi::eXaDrums &drumKit) { 
+    std::cout << "thread loop"; 
+} 
 
 int main() {
     std::cout << "Starting...";
     try {
         eXaDrumsApi::eXaDrums drumKit{"/home/bigbn/data"};
+        
         drumKit.Start([&](int _instrumentId, float _rawVolume) {
-        printf("INST: %d \t VOL:%g\n", _instrumentId, _rawVolume);
+            printf("INST: %d \t VOL:%g\n", _instrumentId, _rawVolume);
         });
+
+        thread func_thread(loop_thread, ref(drumKit));
+        if (func_thread.joinable()) func_thread.join(); 
+        loop_thread(drumKit);
     } catch (const std::exception& e) { 
         std::cout << "Unable to start";    
     }
