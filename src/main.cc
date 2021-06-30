670ed75667b6e715418a9553d2ad1cf4d6e2c554
Pavel Shabardin <pavel@scat.su>
2021-06-30 20:33:53 +0400

    [upd] Debug information

--- src/main.cc
+++ src/main.cc
@@ -3,32 +3,22 @@
 #include <iostream>
 #include <libexadrums/Api/eXaDrums.hpp>
 #include <thread>
+#include <execinfo.h>
 
 // using namespace eXaDrumsApi;
 using namespace std;
 
-void loop_thread(eXaDrumsApi::eXaDrums &drumKit) { 
-    std::cout << "thread loop"; 
-} 
-
 int main() {
-    std::cout << "Starting...";
-    try {
-        eXaDrumsApi::eXaDrums drumKit{"/home/bigbn/data"};
-        
-        drumKit.Start([&](int _instrumentId, float _rawVolume) {
-            printf("INST: %d \t VOL:%g\n", _instrumentId, _rawVolume);
-        });
-
-        thread func_thread(loop_thread, ref(drumKit));
-        if (func_thread.joinable()) func_thread.join(); 
-        loop_thread(drumKit);
-    } catch (const std::exception& e) { 
-        std::cout << "Unable to start";    
-    }
+    std::cout << "Starting...\n";
+    eXaDrumsApi::eXaDrums drumKit{"/home/bigbn/data"};
     
-
-    std::cout << "Started";
-    while(true) { }
+    std::cout << "Constructed...\n";
+    drumKit.Start([&](int _instrumentId, float _rawVolume) {
+        printf("INST: %d \t VOL:%g\n", _instrumentId, _rawVolume);
+    });
+   
+   
+    std::cout << "Started\n";
+    //while(true) { }
     return 0;
 }
